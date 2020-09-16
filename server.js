3c61691f5d9404692f0833ca8936aae8c86a5119
wjml <willian.lourenco@novaandradina.org>
2020-09-15 20:52:44 -0400

    inicializando server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+const path = require('path')
+
+// faz o express procurar na pasta 'public' pelos assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/index.html', function (req, res) {
+   res.sendFile(path.join(__dirname, 'index1.html'));
+});
+
+app.listen(port, function () {
+   console.log('Server is running on http://localhost:' + port);
+});
