10823591c0957e110df568d637ae543b82ac4e27
wjml <willian.lourenco@novaandradina.org>
2020-09-22 16:39:19 -0400

    estilização, estrutura e começo google-sheets

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,82 +1,88 @@
-(function($) {
-  
-  "use strict";  
+(function ($) {
 
-  $(window).on('load', function() {
+  "use strict";
 
-  /*Page Loader active
-    ========================================================*/
+  $(window).on('load', function () {
+
+    /*Page Loader active
+      ========================================================*/
     $('#preloader').fadeOut();
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 200) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
+    // Sticky Nav
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > 200) {
+        $('.scrolling-navbar').addClass('top-nav-collapse');
+        // if($('.nav-item.active')[0].lastElementChild.hash==="#subscription"){
+        //   $('.scrolling-navbar').addClass('top-nav-subscription');
+        // }else{
+        //   $('.scrolling-navbar').removeClass('top-nav-subscription');
+        // }
+      } else {
+          // $('.scrolling-navbar').removeClass('top-nav-subscription');
+          $('.scrolling-navbar').removeClass('top-nav-collapse');
+      }
     });
 
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2020/10/16',function(event){
-      var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Dias</div> '
-      +'<div class="time-entry hours"><span>%H</span> Horas</div> '
-      +'<div class="time-entry minutes"><span>%M</span> Minutos</div> '
-      +'<div class="time-entry seconds"><span>%S</span> Segundos</div> '));
+    jQuery('#clock').countdown('2020/10/16', function (event) {
+      var $this = jQuery(this).html(event.strftime('' +
+        '<div class="time-entry days"><span>%-D</span> Dias</div> ' +
+        '<div class="time-entry hours"><span>%H</span> Horas</div> ' +
+        '<div class="time-entry minutes"><span>%M</span> Minutos</div> ' +
+        '<div class="time-entry seconds"><span>%S</span> Segundos</div> '));
     });
 
     /* slicknav mobile menu active  */
     $('.mobile-menu').slicknav({
-        prependTo: '.navbar-header',
-        parentTag: 'liner',
-        allowParentLinks: true,
-        duplicate: true,
-        label: '',
-      });
+      prependTo: '.navbar-header',
+      parentTag: 'liner',
+      allowParentLinks: true,
+      duplicate: true,
+      label: '',
+    });
 
-      /* WOW Scroll Spy
+    /* WOW Scroll Spy
     ========================================================*/
-     var wow = new WOW({
+    var wow = new WOW({
       //disabled for mobile
-        mobile: false
+      mobile: false
     });
     wow.init();
 
     /* Nivo Lightbox 
     ========================================================*/
     $('.lightbox').nivoLightbox({
-        effect: 'fadeScale',
-        keyboardNav: true,
-      });
+      effect: 'fadeScale',
+      keyboardNav: true,
+    });
 
     // one page navigation 
     $('.navbar-nav').onePageNav({
-            currentClass: 'active'
-    }); 
+      currentClass: 'active'
+    });
 
     /* Back Top Link active
     ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
+    $('.back-to-top').on('click', function (event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    });
 
-  });      
+  });
 
 }(jQuery));
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/index.html', function (req, res) {
-   res.sendFile(path.join(__dirname, 'index1.html'));
+   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, function () {
