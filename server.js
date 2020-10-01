cafe03f57d73989fc28b8f521c3f457008182c33
wjml <willian.lourenco@novaandradina.org>
2020-10-01 11:00:49 -0400

    forcing HTTPS

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const path = require('path')
 
-// faz o express procurar na pasta 'public' pelos assets (css/js/img)
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+   if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
+       res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
+   else //Se a requisição já é HTTPS 
+       next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
