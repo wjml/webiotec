0c4b09055ee0b336ea0b23022850d16cef879f2b
wjml <willian.lourenco@novaandradina.org>
2020-10-05 19:00:42 -0400

    compress add again to test

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-const express = require("express"),
-    app = express(),
-    // compression = require("compression"),
-    port = process.env.PORT || 3e3,
-    path = require("path");
-// app.use(compression()), app.use((req, res, next) => {
-//     (req.headers["x-forwarded-proto"] || "").endsWith("http") ? res.redirect(`https://${req.headers.host}${req.url}`) : next()
-// }), 
-app.use(express.static(__dirname + "/public")), app.get("/", (function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"))
-})), app.get("/index.html", (function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"))
-})), app.listen(port, (function () {
-    console.log("Server is running on http://localhost:" + port)
-}));
+const express=require("express"),app=express(),compression=require("compression"),port=process.env.PORT||3e3,path=require("path");app.use(compression()),app.use((req,res,next)=>{(req.headers["x-forwarded-proto"]||"").endsWith("http")?res.redirect(`https://${req.headers.host}${req.url}`):next()}),app.use(express.static(__dirname+"/public")),app.get("/",(function(req,res){res.sendFile(path.join(__dirname,"index.html"))})),app.get("/index.html",(function(req,res){res.sendFile(path.join(__dirname,"index.html"))})),app.listen(port,(function(){console.log("Server is running on http://localhost:"+port)}));
